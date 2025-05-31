9b32eacc9e042a82f1a330198a14432f1ec8f795
KyoneCode <hadyan.kp@gmail.com>
2025-06-01 04:13:50 +0700

    benerin arah kamera pas spawn

--- src/camera.cpp
+++ src/camera.cpp
@@ -2,8 +2,8 @@
 #include "globals.h"
 
 // Define global variables from globals.h that are primarily camera-related
-float cameraAngleX = 0.0f;
-float cameraAngleY = 30.0f;
+float cameraAngleX = -90.0f;  // Changed to look towards X direction (rotate horizontally)
+float cameraAngleY = -10.0f;  // Changed to look more forward/down instead of up
 float cameraDistance = 15.0f;
 float cameraTargetYOffset = 0.5f;
 
